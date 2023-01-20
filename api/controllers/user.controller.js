f4c8635ef85c3f7a307590af934f7c5fbff026d0
mickael keita <keitamickael@gmail.com>
2023-01-20 16:13:22 +0100

    mis en place de la fonction de suppression d'un utilisateur

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/Users");
-const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 // controller pour recuperer tout les utilisateur
 exports.getAllUsers = async (req, res) => {
@@ -33,5 +33,40 @@ exports.getCurrentUser = async (req, res) => {
     }
   } catch (e) {
     res.status(404).json(null);
+  }
+};
+
+//controller pour supprimer un utilisateur
+exports.deleteCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+
+    if (!user) {
+      res.status(404).json({ message: "L'utilisateur n'existe pas" });
+    } else {
+      // verifie si celui qui fait la requte est celui qui a crée le profil
+      if (req.params.id !== req.auth) {
+        res.status(401).json({ message: "Requete non autorisé" });
+      } else {
+        // recuperation du nom de l'image
+        const filename = user.image.split("/images/")[1];
+
+        // si l'utilisateur a l'mage par defaut, supprime seulement l'utilisateur
+        if (filename === "default.jpg") {
+          await User.deleteOne({ _id: req.params.id });
+          res.status(200).json({ message: "Utilisateur supprimé" });
+        }
+        // sinon supprime tout
+        else {
+          fs.unlink(`images/${filename}`, async () => {
+            await User.deleteOne({ _id: req.params.id });
+            res.status(200).json({ message: "Utilisateur supprimé" });
+          });
+        }
+      }
+    }
+  } catch (e) {
+    // console.log(e);
+    res.status(404).json(e);
   }
 };
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -9,4 +9,7 @@ router.get("/all", userCtrl.getAllUsers);
 //* route pour recuperer l'utilisateur qui fait la requete
 router.get("/", auth, userCtrl.getCurrentUser);
 
+//* route pour supprimer un utilisateuur
+router.delete("/:id", auth, userCtrl.deleteCurrentUser);
+
 module.exports = router;
