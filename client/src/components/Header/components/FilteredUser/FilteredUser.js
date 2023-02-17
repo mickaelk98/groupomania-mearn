629ab51322bc5c2fa147e66b5748c07ab533a783
mickael keita <keitamickael@gmail.com>
2023-02-17 20:40:41 +0100

    rediriger vers la page profil au clique sur l'image un utilisateur

--- client/src/components/Header/components/FilteredUser/FilteredUser.js
+++ client/src/components/Header/components/FilteredUser/FilteredUser.js
@@ -1,9 +1,12 @@
+import { NavLink } from "react-router-dom";
 import style from "./FilteredUser.module.scss";
 
 function FilteredUser({ user }) {
   return (
     <div className={style.user}>
-      <img className={style.image} src={user.image} alt="profil" />
+      <NavLink to={`/profil/${user._id}`}>
+        <img className={style.image} src={user.image} alt="profil" />
+      </NavLink>
       <p className={style.name}>{user.userName}</p>
     </div>
   );
