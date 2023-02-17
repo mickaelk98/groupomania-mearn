81caadafed852a2a67d294d10d2d3cf1ac121cd6
mickael keita <keitamickael@gmail.com>
2023-02-17 15:34:44 +0100

    ajouter un boutton sur la page profil pour la suppression d'un profil

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,12 +1,22 @@
 import Header from "components/Header/Header";
 import ProfilCard from "components/ProfilCard/ProfilCard";
+import { AuthContext } from "context";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
 
 function Profil() {
+  const { user } = useContext(AuthContext);
+  const params = useParams();
+  const userId = params.id;
+
   return (
     <div className="profil">
       <Header />
       <main className="profil__main container">
         <ProfilCard />
+        {user._id === userId && (
+          <button className="profil__main-btn">supprimer le profil</button>
+        )}
       </main>
     </div>
   );
