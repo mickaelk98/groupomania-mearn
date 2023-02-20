2b5ed20a28ef82f237a69d04343edfe55f4b0283
mickael keita <keitamickael@gmail.com>
2023-02-20 11:55:07 +0100

    modification du style de la barre de recherche

--- client/src/components/SearchBox/SearchBox.js
+++ client/src/components/SearchBox/SearchBox.js
@@ -7,36 +7,34 @@ import style from "./SearchBox.module.scss";
 function SearchBox() {
   const [filter, setFilter] = useState("");
   const sortedUsers = useRecoilValue(selectFilteredUsers(filter));
-  const [showSortedUsers, setShowSortedUser] = useState(false);
+  // const [showSortedUsers, setShowSortedUser] = useState(false);
 
   console.log(sortedUsers);
 
   function handleInput(e) {
     setFilter(e.target.value.trim().toLowerCase());
-    setShowSortedUser(true);
   }
   return (
-    <>
-      <div className={style.serchbox}>
-        <button className={style.btnsearch}>
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </button>
+    <div className={style.searchbox}>
+      <form className={style.form}>
         <input
           onInput={handleInput}
           type="text"
-          className={style.inputsearch}
-          placeholder="Rechercher un autre utilisateur"
+          placeholder="Rechercher un utilisateur"
         />
-      </div>
+        <button type="button">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
       <ul className={style.userlist}>
-        {showSortedUsers &&
+        {sortedUsers &&
           sortedUsers.map((user) => (
-            <li key={user._id} onClick={() => setShowSortedUser(false)}>
+            <li key={user._id}>
               <FilteresUser user={user} />
             </li>
           ))}
       </ul>
-    </>
+    </div>
   );
 }
 
