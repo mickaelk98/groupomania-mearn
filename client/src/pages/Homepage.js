f8ded7a21de8209b1f9356c13ae93410d38df158
mickael keita <keitamickael@gmail.com>
2023-02-20 10:35:54 +0100

    deplacer la bare de recherche d'un utilisateur du header a la homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,10 +1,11 @@
-import NewPost from "components/NewPost/NewPost";
-import PostList from "components/PostList/PostList";
 import { useEffect } from "react";
 import { useLoaderData } from "react-router";
 import { useSetRecoilState } from "recoil";
 import { usersState } from "state";
 import Header from "../components/Header/Header";
+import NewPost from "components/NewPost/NewPost";
+import SearchBox from "../components/SearchBox/SearchBox";
+import PostList from "components/PostList/PostList";
 
 function Homepage() {
   const setUsersState = useSetRecoilState(usersState);
@@ -19,6 +20,7 @@ function Homepage() {
       <Header />
       <main className="home__main">
         <NewPost />
+        <SearchBox />
         <PostList />
       </main>
     </div>
