900a7c57128035006c09cfe073c226f3c3993214
mickael keita <keitamickael@gmail.com>
2023-02-23 11:27:05 +0100

    changer la base de donnée selon si nous somme en dev ou prod

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 //* connection a la base de donnée mongoDB via mongoose
 mongoose
-  .connect(`${process.env.MONGODB_DATABASE_LINK}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `${
+      process.env.MODE === "dev"
+        ? process.env.MONGODB_DEV_LINK
+        : process.env.MONGODB_PROD_LINK
+    }`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+// ${process.env.MONGODB_DATABASE_LINK}
